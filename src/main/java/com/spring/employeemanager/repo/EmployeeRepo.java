88eeedd8adba0141016da3dbe5809f924242bff4
Rima Akter <rimawahid3@gmail.com>
2022-06-02 19:16:57 +0600

    first commit

---
+++ src/main/java/com/spring/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,15 @@
+package com.spring.employeemanager.repo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.employeemanager.model.Employee;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long>{
+
+	void deleteEmployeeById(Long id);
+
+	Optional<Employee> findEmployeeById(Long id);
+
+}
